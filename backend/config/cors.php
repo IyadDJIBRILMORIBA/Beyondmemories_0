8a5dd058f3a7b14f50f09176d934faeb94587cf6
@onesimeaht <onesime.ahoton@epitech.eu>
2025-11-29 22:05:52 +0100

    feat: our configuration etablishment

---
+++ backend/config/cors.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'], // En prod, mettre l'URL exacte du front
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
